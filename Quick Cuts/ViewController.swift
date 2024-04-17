96be888bfc29f732ea27547909ab0bd9d3fdf120
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-04-17 14:44:14 +0530

    Initial Commit

---
+++ Quick Cuts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quick Cuts
+//
+//  Created by Akshay Jha on 17/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
