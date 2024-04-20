a4483db2e0161bc81a61cef3d6fc6c841c5f5db4
Amitdhal007 <amit0162.be21@chitkara.edu.in>
2024-04-20 15:57:01 +0530

    splash screen and logo added

---
+++ Quick Cuts/Helpers/ViewExtensions.swift
@@ -0,0 +1,25 @@
+import Foundation
+import UIKit
+
+extension UITextField{
+   @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: newValue!])
+        }
+    }
+}
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = true
+        }
+    }
+}
