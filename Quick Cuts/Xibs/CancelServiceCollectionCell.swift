962583353b7139b1c8ef474008a2ee4076376ecb
Amitdhal007 <amit0162.be21@chitkara.edu.in>
2024-04-23 21:09:38 +0530

    cancel booking screen and select date and time screen added

---
+++ Quick Cuts/Xibs/CancelServiceCollectionCell.swift
@@ -0,0 +1,19 @@
+//
+//  CancelServiceCollectionCell.swift
+//  Quick Cuts
+//
+//  Created by Amit Kumar Dhal on 23/04/24.
+//
+
+import UIKit
+
+class CancelServiceCollectionCell: UICollectionViewCell {
+
+    @IBOutlet weak var cancelReasonLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
