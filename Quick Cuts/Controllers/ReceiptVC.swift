94feb46c5fde956c62a78d1a0c40f9e62101bfad
Neeraj Sharma <ns98054978@gmail.com>
2024-05-02 10:16:00 +0530

    Added Receipt Page

---
+++ Quick Cuts/Controllers/ReceiptVC.swift
@@ -0,0 +1,63 @@
+
+import UIKit
+
+class ReceiptVC: UIViewController {
+    private var contentSizeObservation:NSKeyValueObservation?
+    @IBOutlet weak var checkout: UITableView!{
+        
+        didSet{
+            checkout.registerCellFromNib(cellID: "CheckoutTableViewCell")
+//            contentSizeObservation = checkout.observe(\.contentSize, options: [.new]) { [weak self] checkout, change in
+//                self?.checkout.invalidateIntrinsicContentSize()
+//                self?.heightobserver.constant = checkout.contentSize.height
+//                self?.view.layoutIfNeeded()
+//            }
+            
+        }
+    }
+    
+//    @IBOutlet weak var heightobserver: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        checkout.sectionHeaderTopPadding = 0
+    }
+    
+
+
+}
+
+extension ReceiptVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckoutTableViewCell", for: indexPath) as! CheckoutTableViewCell
+        return cell
+        
+        
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard section >= 0 else { return nil }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dummyTableCell") as? dummyTableCell
+        
+        guard let validCell = cell else {
+            let newCell = dummyTableCell()
+            return newCell.contentView
+        }
+        
+        return validCell.contentView
+    }
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    
+    
+}
