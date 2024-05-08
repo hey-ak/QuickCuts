a24d6c99f0eaf11e3401afa7d5796a6932c19813
Neeraj Sharma <ns98054978@gmail.com>
2024-05-08 15:37:09 +0530

    review page added

---
+++ Quick Cuts/Controllers/ReviewVC.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class ReviewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
